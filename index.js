1ef5cee5c7dbce8c4fd5ba1d0681e2d136a7f089
James Halliday <mail@substack.net>
2014-05-26 16:46:37 -0700

    a different approach

--- index.js
+++ index.js
@@ -17,59 +17,30 @@ function format (x, bytes) {
         if (bytes === 1) return undefined;
         return sprintf(rfmt, bytes >= 9 ? '-Infinity' : '-Inf').slice(0, bytes);
     }
-    
-    var n, b = Math.ceil((bytes + 1) / 2);
-    if (x === 0) n = 0
-    else n = Math.floor(Math.log(Math.abs(x)) / Math.log(10))
-    
-    if (n >= Math.ceil((bytes - 1) / 2)) return sci(x, n, bytes);
-    if (n < -2) return sci(x, n, bytes);
-    
-    var y = x < 10 ? nround(x, n, bytes) : x;
-    var rbytes = Math.floor((bytes - 1) / 2);
-    var res = sprintf('%' + bytes + '.' + rbytes + 'f', y);
-    var dot = res.split('.')[1];
-    var ex = Math.floor((bytes - 1) / 2)
-    if (!/^[ -]/.test(res) && dot && dot.length + 1 !== ex) res = ' ' + res;
-    return res.slice(0, bytes);
+    var s = packf(x, bytes);
+    if (s === undefined) return s;
+    return s.slice(0, bytes);
 };
 
-function sci (x, n, bytes) {
+function sci (x, bytes) {
+    var n = Math.floor(Math.log(Math.abs(x)) / Math.log(10));
     var p = 'e' + String(n);
-    if (n < 0) {
-        var s = format(x * Math.pow(10,-n), bytes - p.length);
-        if (s === undefined) return s;
-        if (/^[\s-]\d{2,}/.test(s) && x < 1) {
-            p = 'e' + String(n - 1);
-            s = format(x * Math.pow(10,-n-1), bytes - p.length);
-        }
-        return s + p;
-    }
-    var s, y = x / Math.pow(10, n);
+    var y = Math.abs(x);
     
-    var b = Math.ceil(Math.log(Math.abs(x)) / Math.log(10));
-    var sx = Math.pow(10, -(bytes - b - 2));
-    var slo = Math.pow(10,b) - sx;
-    var sup = Math.pow(10,b) - sx * 0.5
-    if (Math.abs(x) < sup && Math.abs(x) > slo) {
-        return (x < 0 ? '-' : ' ') + String(slo);
-    }
-    if (Math.abs(y) + Math.pow(10, p.length - bytes + rounder) >= 10) {
-        s = format(y / 10, bytes - p.length - Math.floor((bytes + 1) % 2));
-        p = 'e' + String(n + 1);
-    }
-    else s = format(y, bytes - p.length);
-    
+    var s = packf(x / Math.pow(10, n), bytes - p.length);
     if (s === undefined) return s;
-    
-    var res = (s + p).slice(0, bytes);
-    var extra = Array(bytes - res.length + 1).join(' ');
-    return extra + res;
+    return s + p;
 }
 
-function nround (x, n, bytes) {
-    var sign = x < 0 ? -1 : 1;
-    var pb = Math.pow(10, Math.floor(bytes / 2) - Math.max(0, n + sign));
-    if (x > 0 && x <= 2) pb *= 10;
-    return sign * Math.round(Math.abs(x) * pb) / pb;
+function packf (x, bytes) {
+    var lbytes = Math.floor(bytes / 2 - 1);
+    var rbytes = bytes - lbytes - 2;
+    
+    var s = Math.abs(x).toFixed(rbytes);
+    var dec = s.split('.')[0];
+    if (lbytes < dec.length - 1) {
+        return sci(x, bytes);
+    }
+    var sp = Array(lbytes - dec.length + 1).join(' ');
+    return sp + (x < 0 ? '-' : ' ') + s;
 }

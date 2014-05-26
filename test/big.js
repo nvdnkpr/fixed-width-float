910936447b05b7b62a058fabba0bb90d05d3f933
James Halliday <mail@substack.net>
2014-05-26 00:52:10 -0700

    failing negative big tests

--- test/big.js
+++ test/big.js
@@ -17,3 +17,20 @@ test('big', function (t) {
     
     t.end();
 });
+
+test('negative big', function (t) {
+    t.equal(showf(-       1, 7), ' -1.000');
+    t.equal(showf(-      10, 7), '-10.000');
+    t.equal(showf(-     100, 7), '-1.00e2');
+    t.equal(showf(-    1000, 7), '-1.00e3');
+    t.equal(showf(-   10000, 7), '-1.00e4');
+    t.equal(showf(-  100000, 7), '-1.00e5');
+    t.equal(showf(- 1000000, 7), '-1.00e6');
+    t.equal(showf(-10000000, 7), '-1.00e7');
+    
+    t.equal(showf(-1.23456e9, 7), '-1.23e9');
+    t.equal(showf(-1.23456e20, 7), '-1.2e20');
+    t.equal(showf(-1.26456e20, 7), '-1.3e20');
+    
+    t.end();
+});

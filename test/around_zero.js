d4080c0c00fe9398117e2cd6eb57cdc9b8caad3a
James Halliday <mail@substack.net>
2014-05-26 05:20:54 -0700

    test fixes for leading space

--- test/around_zero.js
+++ test/around_zero.js
@@ -6,7 +6,7 @@ test('around zero', function (t) {
     t.equal(showf(0, 7), '  0.000');
     t.equal(showf(-1, 7), ' -1.000');
     
-    t.equal(showf(1, 3), '1.0');
+    t.equal(showf(1, 3), ' 1.');
     t.equal(showf(-1, 3), '-1.');
     
     t.equal(showf(1, 14), '      1.000000');

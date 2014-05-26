decd820862300ddff07fe17b86be2436a0764df3
James Halliday <mail@substack.net>
2014-05-25 22:29:14 -0700

    some failing cases

---
+++ test/show.js
@@ -0,0 +1,8 @@
+var showf = require('../');
+var test = require('tape');
+
+test('small numbers', function (t) {
+    t.equal(showf(1, 7), '  1.000');
+    t.equal(showf(0, 7), '  0.000');
+    t.equal(showf(-1, 7), ' -1.000');
+});
